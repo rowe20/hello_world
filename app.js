c0cf44debdc2ede60349dd11a111182be3deb395
rowe20 <69379504+rowe20@users.noreply.github.com>
2021-12-06 08:19:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+//checking
+app.use(express.json());
+const port = 5000;
+const mongoose = require("mongoose");
  
+mongoose
+.connect(process.env.MONGOURL)
+.then( ()=> console.log("Mongo DB connected"));
+
+
 // Getting Request
-app.get('/', (req, res) => {
+
+app.get("/",(req,res)=> res.send("Welcome To Anime World"));
+/*app.get('/', (req, res) => {
  
     // Sending the response
     res.send('Hello World!')
     
     // Ending the response
     res.end()
-})
+})*/
  
 // Establishing the port
 const PORT = process.env.PORT ||5000;
